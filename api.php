1ca79cca276d817f25893cb7c653d3427ec2d375
kmoz000 <ouazmir@gmail.com>
2018-09-14 00:00:02 +0100

    Update api.php

--- api.php
+++ api.php
@@ -3,12 +3,12 @@
     if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        header('Access-Control-Max-Age: 86400');    // 1 day age
     }
 
-    // Access-Control headers are received during OPTIONS requests
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
+    // Access-Control headers 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             header("Access-Control-Allow-Methods: POST");         
 
@@ -22,9 +22,9 @@
         return new mysqli("localhost",base64_decode($username), base64_decode($password),base64_decode($database));
     }
 // Check connection
-    $postdata = file_get_contents("php://input");
-    if (isset($postdata)) {
-        $request = json_decode($postdata);
+    $data = file_get_contents("php://input");
+    if (isset($data)) {
+        $request = json_decode($data);
         $action = $request->action;
         $sql = $request->sql;
         $database = $request->database;
@@ -33,19 +33,19 @@
 
 switch ($action){
     case "query_get":
-    $conn=con($username,$password,$database);$res = $conn->query($sql);echo $res;$conn->close();
+    $conn=con($username,$password,$database);$res = $conn->query($sql);print_r($res);$conn->close();
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     };
     break;
     case "multi_query_set":
-    $conn=con($username,$password,$database);$res = mysqli_query($conn, $sql);echo $res;mysqli_close($conn);
+    $conn=con($username,$password,$database);$res = mysqli_query($conn, $sql);print_r($res);mysqli_close($conn);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     };
     break;
     case "query_set":
-    $conn=con($username,$password,$database);$res = $conn->query($sql);echo $res;$conn->close();
+    $conn=con($username,$password,$database);$res = $conn->query($sql);print_r($res);$conn->close();
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     };
